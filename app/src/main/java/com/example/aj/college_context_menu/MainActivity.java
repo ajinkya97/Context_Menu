7682bcd248d554171adc689a157e8b2d5aac636f
59436419 <kb@ksyko.com>
2017-09-20 00:06:35 +0530

    Fix Image ScaleType

--- app/src/main/java/com/example/aj/college_context_menu/MainActivity.java
+++ app/src/main/java/com/example/aj/college_context_menu/MainActivity.java
@@ -18,38 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         img=(ImageView)findViewById(R.id.myimage);
         registerForContextMenu(img);
-
     }
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
-    {
+
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         MenuInflater inflater=getMenuInflater();
         inflater.inflate(R.menu.menu_main,menu);
     }
-    public boolean onContextItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case R.id.center:
-                img.setScaleType(ImageView.ScaleType.CENTER);
-                Toast.makeText(this, "center", Toast.LENGTH_SHORT).show();
-                return true;
+                img.setScaleType(ImageView.ScaleType.CENTER); break;
             case  R.id.fit_xy:
-                img.setScaleType(ImageView.ScaleType.FIT_XY);
-                Toast.makeText(this, "fit xy", Toast.LENGTH_SHORT).show();
-                return true;
+                img.setScaleType(ImageView.ScaleType.FIT_XY); break;
             case  R.id.fit_end:
-                img.setScaleType(ImageView.ScaleType.FIT_END);
-                Toast.makeText(this, "fit end", Toast.LENGTH_SHORT).show();
-                return true;
+                img.setScaleType(ImageView.ScaleType.FIT_END); break;
             case  R.id.fit_start:
-                img.setScaleType(ImageView.ScaleType.FIT_START);
-                Toast.makeText(this, "fit start", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onContextItemSelected(item);
+                img.setScaleType(ImageView.ScaleType.FIT_START); break;
         }
+        Toast.makeText(this, img.getScaleType().toString(), Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
